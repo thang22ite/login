5af7930a64cecc14f8d372a97295204f7b04eebe
thang22ite <levanrom87@gmail.com>
2026-04-15 13:26:32 +0700

    update something

--- lib/common/blocs/auth/auth_cubit.dart
+++ lib/common/blocs/auth/auth_cubit.dart
@@ -1,11 +1,10 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
 import 'package:vd_login/common/blocs/auth/auth_state.dart';
+import 'package:vd_login/core/di/injection.dart';
 import 'package:vd_login/domain/usecases/get_token_usecase.dart';
 import 'package:vd_login/domain/usecases/logout_usecase.dart';
 
 class AuthCubit extends Cubit<AuthState>{
-  final sl = GetIt.instance;
 
   AuthCubit() : super(AppInitialState());
 
--- lib/presentation/cubit/login/login_cubit.dart
+++ lib/presentation/cubit/login/login_cubit.dart
@@ -1,23 +1,18 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
+import 'package:vd_login/core/di/injection.dart';
 import 'package:vd_login/domain/usecases/login_usecase.dart';
 import 'package:vd_login/domain/usecases/save_token_usecase.dart';
 import 'package:vd_login/presentation/cubit/login/login_state.dart';
 
 class LoginCubit extends Cubit<LoginState>{
-  final sl = GetIt.instance;
-
 
   LoginCubit() : super (LoginInitial());
 
   Future<void> login(String email, String password)async{
     emit(LoginLoading());
     try{
-      final loginUsecase  = sl<LoginUsecase>();
-      final saveTokenUsecase = sl<SaveTokenUsecase>();
-
-      final user = await loginUsecase(email,password);
-      await saveTokenUsecase(user.accessToken);
+      final loginUsecase  = await sl<LoginUsecase>()(email, password);
+      await sl<SaveTokenUsecase>()(loginUsecase.accessToken);
       emit(LoginSuccess());
     }catch (e) {
       emit(LoginError(e.toString()));
